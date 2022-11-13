ca920e6d8c35e8cb9eae6a3a7ba1260c96b8ab68
komeno <tkomeno@student.42tokyo.jp>
2022-11-13 16:38:08 -0300

    including "readline" to .h

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: tkomeno <tkomeno@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 20:39:16 by tkomeno           #+#    #+#             */
-/*   Updated: 2022/11/12 20:39:44 by tkomeno          ###   ########.fr       */
+/*   Updated: 2022/11/13 11:38:23 by tkomeno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
 #endif
