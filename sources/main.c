33bb0a5b5f690233b2fca4c7278603dfe2be56a5
komeno <tkomeno@student.42tokyo.jp>
2022-11-29 19:32:48 -0300

    executing commands

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: tkomeno <tkomeno@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 20:38:41 by tkomeno           #+#    #+#             */
-/*   Updated: 2022/11/29 18:54:19 by tkomeno          ###   ########.fr       */
+/*   Updated: 2022/11/29 19:31:01 by tkomeno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,30 +58,71 @@ t_env	*prepare_environment(char **envp)
 	return (env_lst);
 }
 
+void append_slash_to_path(char **paths)
+{
+	int i;
+
+	i = -1;
+	while (paths[++i])
+		paths[i] = ft_strjoin(paths[i], "/");
+}
+
+char **get_paths(t_env *env_lst)
+{
+	char *path;
+	char **paths;
+
+	while (!env_lst->is_sentinel)
+	{
+		if (ft_strncmp(env_lst->key, "PATH", 5) == 0)
+			path = env_lst->value;
+		env_lst = env_lst->next;
+	}
+	paths = ft_split(path, ':');
+	append_slash_to_path(paths);
+	return (paths);
+}
+
+char *find_command(char *command, char **paths)
+{
+	int i;
+	char *tmp;
+	char *path;
+
+	path = NULL;
+	if (access(command, F_OK | X_OK) == 0)
+		return (command);
+	i = -1;
+	while (paths[++i])
+	{
+		tmp = ft_strjoin(paths[i], command);
+		if (access(tmp, F_OK | X_OK) == 0)
+		{
+			path = tmp;
+			break ;
+		}
+		free(tmp);
+	}
+	return (path);
+}
+
 int	main(int argc, char **argv, char **envp)
 {
+	char *line;
+	char **tokens;
+	char **paths;
 	t_env *env_lst;
 
 	void_caster(argc, argv, envp);
 	env_lst = prepare_environment(envp);
+	paths = get_paths(env_lst);
 
-	for (t_env *tmp = env_lst; !tmp->is_sentinel; tmp = tmp->next)
-		printf("%s : %s\n", tmp->key, tmp->value);
-
+	while (true)
+	{
+		line = read_line();
+		tokens = tokenizer(line);
+		line = find_command(tokens[0], paths);
+		execve(line, tokens, NULL);
+	}
 	return (0);
 }
-// int	main(int argc, char **argv, char **envp)
-// {
-// 	char *line;
-// 	char **tokens;
-// 	char *path;
-
-// 	void_caster(argc, argv, envp);
-
-// 	while (true)
-// 	{
-// 		line = read_line();
-// 		tokens = tokenizer(line);
-// 	}
-// 	return (0);
-// }
